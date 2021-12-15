3bf6009310f75fb96cf52c836f92ca50a64a5c2d
viveknimbolkar <viveknimbolkar50@gmail.com>
2021-12-15 08:46:43 +0530

    validate user input in index page

--- index.php
+++ index.php
@@ -5,11 +5,16 @@ require "backend/connection.php";
 //check the index content
 if (isset($_GET["send_text"])) {
     $text = mysqli_real_escape_string($conn,$_GET["user_text"]);
-    $key = uniqid();
-    $insert_query = "INSERT INTO `content` (`user_key`, `writeup`) VALUES ('$key', '$text')";
-    $send_info = mysqli_query($conn,$insert_query);
-    if ($send_info) {
-        header("location: view.php?q=$key");
+    if($text == ""){
+        echo "<script> alert('Quoute should not be empty');</script>";
+        header("location: /");
+    }else{
+        $key = uniqid();
+        $insert_query = "INSERT INTO `content` (`user_key`, `writeup`) VALUES ('$key', '$text')";
+        $send_info = mysqli_query($conn,$insert_query);
+        if ($send_info) {
+            header("location: view.php?q=$key");
+        }
     }
 }
 
@@ -21,6 +26,7 @@ if (isset($_GET["send_text"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Write your thoughts and share with anyone you want.">
     <title>Write & Share</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -31,7 +37,7 @@ if (isset($_GET["send_text"])) {
     <header class="bg-primary p-2 text-white">
         <center>
             <strong>
-                <h2>WRITE & SHARE</h2>
+               <h1>WRITE & SHARE</h1>
             </strong>
         </center>
     </header>
@@ -39,7 +45,7 @@ if (isset($_GET["send_text"])) {
     <section class="container mt-4">
         <center>
             <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="get">
-                <textarea name="user_text" id="maintext" placeholder="Write anything that you want..."></textarea>
+                <textarea name="user_text" id="maintext" placeholder="Write anything..."></textarea>
                 <input class="btn btn-danger" type="reset" value="Reset">
                 <button class="btn btn-success" type="submit" name="send_text"><i class="fas fa-paper-plane"></i> Submit</button>
             </form>
--- view.php
+++ view.php
@@ -12,21 +12,28 @@ require_once "backend/connection.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Write & Share</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="backend/style.css">
 </head>
+
 <body>
     <header class="bg-primary p-2 text-white">
         <center>
             <strong>
-                <a href="index.php"><h2>WRITE & SHARE</h2></a>
+                <a href="/">
+                    <h1>WRITE & SHARE</h1>
+                </a>
             </strong>
         </center>
     </header>
@@ -34,31 +41,37 @@ require_once "backend/connection.php";
 
     <section class="container mt-1">
         <center>
-            <input type="text" class="m-3" id="content_link" value="<?php echo $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>">
+            <input type="text" class="m-3" id="content_link"
+                value="<?php echo $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>">
             <button class="btn m-2 btn-primary" onclick="copyURL()"><i class="far fa-copy"></i> Copy link</button>
 
             <textarea name="user_text" id="maintext">
                     <?php
                        while ($row = mysqli_fetch_assoc($find_content)) {
                            echo $row["writeup"];
-                       }//end of while
-                    }//end of row
+                       }
+                    }
                     else{
                         header("location: index.php");
-                    }//end of fetch
+                    }
                     
             }
                     ?>
                 </textarea>
-                <h4>Also share on:</h4>
-                <a href="https://www.facebook.com/"><button class="btn btn-primary"><i class="fab fa-facebook"></i></button></a>
-                <a href="https://wa.me/"><button class="btn btn-success"><i class="fab fa-whatsapp"></i></button></a>
-                <a href="https://t.me/"><button class="btn btn-primary"><i class="fab fa-telegram"></i></button></a>
+            <h4>Also share on:</h4>
+            <a href="https://www.facebook.com/"><button class="btn btn-primary"><i
+                        class="fab fa-facebook"></i></button></a>
+            <a href="https://wa.me/?text=urlencodedtext" id="whatsapp"><button class="btn btn-success"><i
+                        class="fab fa-whatsapp"></i></button></a>
+            <a href="https://t.me/"><button class="btn btn-primary"><i class="fab fa-telegram"></i></button></a>
         </center>
     </section>
-    
+
 
     <script>
+        //create whatsapp link
+        var content = document.getElementById("maintext").value;
+        document.getElementById("whatsapp").setAttribute("href", "https://wa.me/?text=" + content);
 
         //adssign page address to the paragraph tag
         function copyURL() {
@@ -69,7 +82,10 @@ require_once "backend/connection.php";
         }
     </script>
 
-    
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
